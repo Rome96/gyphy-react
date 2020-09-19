c72b2432358ef3e8049d6059f92dd65656c967c9
Turiano Romero <romero.guzman18@gmail.com>
2020-09-19 00:35:09 -0500

    create and configure project, create component GifExpertApp

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+const GifExpertApp = props => {
+  return (
+    <>
+      <h2>Gif Expert App</h2>
+    </>
+  )
+}
+
+GifExpertApp.propTypes = {
+
+}
+
+export default GifExpertApp

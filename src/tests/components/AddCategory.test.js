2e9a71f9836fb0bdb61853c7c487b4bd8c5d9033
Turiano Romero <romero.guzman18@gmail.com>
2020-09-22 19:34:46 -0500

    onChange input search in component AddCategory

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -5,6 +5,7 @@ import AddCategory from '../../components/AddCategory'
 
 describe("Testing in components <AddCategory/>", () => {
   const setCategories = () => {};
+
   let wrapper = shallow(<AddCategory setCategories={setCategories} />);
 
   beforeEach(() => {
@@ -14,4 +15,12 @@ describe("Testing in components <AddCategory/>", () => {
   test("Render component <AddCategory />", () => {
     expect(wrapper).toMatchSnapshot()
   });
+
+  test('onChange input search', () => {
+    const input = wrapper.find('input')
+    const value = 'Hello World'
+    input.simulate('change', { target: { value } })
+
+  });
+
 });

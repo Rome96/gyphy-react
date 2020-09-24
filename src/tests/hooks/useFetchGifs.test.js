c90793f6cccc6fb4a1d9280b32f614bfcda4de6e
Turiano Romero <romero.guzman18@gmail.com>
2020-09-24 00:21:01 -0500

    Testing CustomHook useFetchGifs. waitForNextUpdate

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -3,11 +3,22 @@ import { useFetchGifs } from '../../hooks/useFetchGifs';
 import { renderHook }  from '@testing-library/react-hooks';
 
 describe('Testing Hook useFetchGifs', () => {
-  test('', () => {
-    const { result } = renderHook(() => useFetchGifs('Naruto')); // emula un functional component
+
+  test('Return state initial', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('Naruto')); // emula un functional component
     const { data, loading } = result.current; // devuelve el state actual del hook
+    await waitForNextUpdate()
 
     expect(data).toEqual([]);
     expect(loading).toBe(true);
+  });
+
+  test("Return array of images and loading false", async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs("Naruto"));
+    await waitForNextUpdate()
+    const { data, loading } = result.current; // devuelve el state actual del hook
+
+    expect(data.length).toEqual(5);
+    expect(loading).toBe(false);
   });
 });

9ac85daa40d2cbda50e45c87f9797723383be8fc
Turiano Romero <romero.guzman18@gmail.com>
2020-09-23 20:18:59 -0500

    Testing in component <GifGrid/> Mock customHook

--- src/tests/components/GifGrid.test.js
+++ src/tests/components/GifGrid.test.js
@@ -2,17 +2,35 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { shallow } from 'enzyme'
 import GifGrid from '../../components/GifGrid';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+
+jest.mock('../../hooks/useFetchGifs'); // emular llamado al hook useFetchGifs :)
 
 describe('Testing in component <GifGrid/>', () => {
   const category = 'Goku';
-  let wrapper = shallow(<GifGrid category={category} />);
-
-  beforeEach(() => {
-    wrapper = shallow(<GifGrid category={category} />);
-  });
 
   test('Render component  <GifGrid/>', () => {
+    useFetchGifs.mockReturnValue({
+      data: [],
+      loading: true,
+    });
+    const wrapper = shallow(<GifGrid category={category} />);
     expect(wrapper).toMatchSnapshot()
   });
-  
+
+  test('Render <GifGridItem/> when images are loaded of hook useFetchGifs', () => {
+    const gifs = [{
+      id: '1',
+      title: 'Naruto',
+      urlImg: 'https://localhost/data/naruto.gif'
+    }];
+
+    useFetchGifs.mockReturnValue({
+      data: gifs,
+      loading: false,
+    });
+    const wrapper = shallow(<GifGrid category={category} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
 });
